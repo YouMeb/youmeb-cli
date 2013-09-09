d04ba1f7e12302dcd05aeaa3ed1fd5e7a5fac9e2
Po-Ying Chen <poying.me@gmail.com>
2013-09-09 22:34:31 +0800

    丟掉 cliox 直接用 cliopt

--- bin/youmeb.js
+++ bin/youmeb.js
@@ -1,48 +1,160 @@
 #!/usr/bin/env node
 
+// 設定 process.title
+process.title = 'youmeb';
+
 var fs = require('fs');
 var path = require('path');
-var cliox = require('cliox');
-var app = cliox();
-var dir, pkg, youmeb, done;
+var cliopt = require('cliopt');
+var colors = require('colors');
+var commands = require('../lib/commands');
+var dir, pkg, clipkg, cli, exists, done;
+
+// 定義 cli options
+var parser = cliopt({
+  help: {
+    type: Boolean,
+    default: false
+  },
+  version: {
+    type: Boolean,
+    default: false
+  },
+  root: {
+    type: String,
+    default: process.cwd()
+  }
+});
+
+parser.use(cliopt.pair());
+parser.use(cliopt.convert());
 
 dir = path.join(process.cwd(), 'node_modules/youmeb');
 
-if (!fs.existsSync(dir)) {
-  console.log('Unable to find local youmeb.');
-  process.exit(1);
+exists = fs.existsSync(dir);
+
+if (exists) {
+  // 取得 package.json
+  pkg = require(path.join(dir, 'package.json'));
+
+  // 載入 youmeb-cli
+  cli = require(path.join(dir, 'lib/youmeb-cli.js'));
 }
 
-// 取得 package.json
-pkg = require(path.join(dir, 'package.json'));
+clipkg = require(path.join(__dirname, '../package.json'));
 
-// 設定 process.title
-process.title = pkg.name;
-
-// 載入 youmeb-cli
-youmeb = require(path.join(dir, 'lib/youmeb-cli.js'));
-
-// cliox
-app
-  .version(pkg.version, ['-V'])
-  .verbose(['-v'])
-  .format(['json'])
-  .help(path.join(dir, 'docs/cli'), 'help.%s.txt');
-
-app.bind(youmeb);
-
-done = function (err) {
+var done = function (err) {
   if (err) {
-    console.log('Error: ' + err.message);
+    console.log('\n  Error: '.red + (err.message || 'no message') + '\n');
   }
 };
 
-app.noCommand(function () {
-  app.run(['server', './'], done);
-});
+parser.parse(process.argv.slice(2), function (err) {
+  if (err) {
+    throw err;
+  }
 
-app.notFound(function (command, app, done) {
-  return youmeb.notfound(command, app, done);
-});
+  // version
+  if (parser.get('version')) {
+    console.log('cli version: ' + clipkg.version);
+    if (pkg) {
+      console.log('youmeb version: ' + pkg.version);
+    }
+    return done();
+  }
+  
+  var command = parser.args.shift();
 
-app.run(process.argv.slice(2), done);
+  // help
+  if (parser.get('help')) {
+    var data = {
+      header: ['  youmeb [options] [command]'],
+      // [name, args, msg]
+      commands: [
+        ['new', '[app]', 'create new applocation']
+      ]
+    };
+
+    (function (log) {
+      if (cli) {
+        cli.youmeb.emit('help', command, data, log);
+      } else {
+        log();
+      }
+    })(function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      var txtlen = [];
+      var str;
+      
+      data.commands.forEach(function (item) {
+        item.forEach(function (field, i) {
+          var len = field.length;
+          if (txtlen[i] | 0 < len) {
+            txtlen[i] = len;
+          }
+        });
+      });
+
+      data.commands.forEach(function (item, i) {
+        item.forEach(function (field, j) {
+          var k = txtlen[j] - field.length;
+          while (k--) {
+            field += ' ';
+          }
+          switch (j) {
+            case 0:
+              field = '    ' + field.green;
+              break;
+            case 1:
+              field = field.grey;
+              break;
+          }
+          item[j] = field;
+        });
+        data.commands[i] = item.join('  ');
+      });
+
+      str = data.commands.join('\n');
+
+      str = '\n'
+        + data.header.join('\n') + '\n\n'
+        + '  Commands:\n\n'
+        + str + '\n';
+
+      console.log(str);
+      done();
+    });
+    return;
+  }
+
+  // 沒有 command
+  if (!command) {
+    if (cli) {
+      cli.server(parser, parser.args, done);
+    } else {
+      done();
+    }
+    return;
+  }
+  
+  // 檢查 youmeb-cli 是否有這個 command
+  if (commands.hasOwnProperty(command)) {
+    return commands[command](parser.args, done);
+  }
+
+  // 使用 youmeb 的 command 
+  if (cli) {
+    cli.boot(function (err) {
+      if (err) {
+        return done(err);
+      }
+      cli.youmeb.emit('cli-' + command, parser, parser.args, done);
+    });
+    return;
+  }
+
+  done();
+});
